389329f85396d82a92f8da3c81f36b5035b6c8f4
Yura <shelkanin09@gmail.com>
2022-06-20 15:17:06 +0300

    Настройки для Хероку_2

--- scrapping/urls.py
+++ scrapping/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
